5eed58096eaf2297502ab3c4f88e81f844b48f1e
theduke <chris@theduke.at>
2015-11-12 10:46:43 +0100

    Implemented setup/teardown + beforeEach/afterEach

--- omega.nim
+++ omega.nim
@@ -7,11 +7,16 @@ type
   SkipError = object of Exception
     discard
 
-  TestError = object of Exception
-    details*: string  
+  BeforeEachError = object of Exception
+    description: string
+    error: ref Exception
+
+  AfterEachError = object of Exception
+    description: string
+    error: ref Exception
 
   TestStatus = enum
-    SUCCESS, SKIPPED, ERROR
+    UNKNOWN, BEFORE_EACH_ERR, SUCCESS, SKIPPED, ERROR, AFTER_EACH_ERR
 
   OmegaTest = ref object of RootObj
     name: string
@@ -64,6 +69,9 @@ type
     suite*: string
     descriptions*: seq[DescriptionResult]
     testCount*: int
+
+    error: ref Exception
+
     skipped*: int
     succeeded*: int
     failed*: int
@@ -95,6 +103,34 @@ proc fullName(desc: OmegaDescription): string =
 
   return name
 
+proc runBeforeEach(this: OmegaDescription) =
+  if this.parent != nil:
+    this.parent.runBeforeEach()
+
+  if this.beforeEach != nil:
+    try:
+      this.beforeEach()
+    except:
+      var e = getCurrentException()
+      var newE = newException(BeforeEachError, this.fullName & ".beforeEach() failed: \n" & e.msg)
+      newE.description = this.name
+      newE.error = e
+      raise newE
+
+proc runAfterEach(this: OmegaDescription) =
+  if this.afterEach != nil:
+    try:
+      this.afterEach()
+    except:
+      var e = getCurrentException()
+      var newE = newException(AfterEachError, this.fullName & ".afterEach() failed:\n" & e.msg)
+      newE.description = this.name
+      newE.error = e
+      raise newE
+
+  if this.parent != nil:
+    this.parent.runAfterEach()
+
 # Suite procs.
 
 proc testCount(suite: OmegaSuite): int =
@@ -111,9 +147,9 @@ type Reporter = ref object of RootObj
 
 type
   Handler = ref object of RootObj
-    suites*: seq[OmegaSuite]
-    randomizeSuites*: bool
-    runParallel*: int
+    suites: seq[OmegaSuite]
+    randomizeSuites: bool
+    runParallel: int
     reporters: seq[Reporter] 
 
 
@@ -146,6 +182,12 @@ method onFinish(this: Reporter, res: OmegaResult) {.base.} =
 
 # Handler base methods
 
+proc registerReporter(this: Handler, r: Reporter) =
+  this.reporters.add(r)
+
+proc addSuite(this: Handler, s: OmegaSuite) =
+  this.suites.add(s)
+
 proc testCount(handler: Handler): int =
   var count = 0
   for suite in handler.suites:
@@ -169,11 +211,19 @@ proc runTest(this: Handler, test: OmegaTest, suite: OmegaSuite, description: Ome
 
   # Run test.
   try:
+    description.runBeforeEach()
     test.test()
+    description.runAfterEach()
     res.status = SUCCESS
+  except BeforeEachError:
+    res.status = BEFORE_EACH_ERR
+    res.error = getCurrentException()
   except SkipError:
     res.status = SKIPPED
     res.skipReason = getCurrentExceptionMsg()
+  except AfterEachError:
+    res.status = AFTER_EACH_ERR
+    res.error = getCurrentException()
   except:
     res.error = getCurrentException()
     res.status = ERROR
@@ -218,8 +268,10 @@ proc runDescription(this: Handler, descr: OmegaDescription, suite: OmegaSuite, p
       res.succeeded += 1
     of SKIPPED:
       res.skipped += 1
-    of ERROR:
+    of ERROR, BEFORE_EACH_ERR, AFTER_EACH_ERR:
       res.failed += 1
+    of UNKNOWN:
+      raise newException(Exception, "Unknown test status")
 
   res.timeTaken = epochTime() - startTime
 
@@ -233,16 +285,32 @@ proc runSuite(this: Handler, suite: OmegaSuite): SuiteResult =
     r.onSuiteStarted(suite)
 
   var startTime = epochTime()
-  var res = SuiteResult(descriptions: @[])
-  for d in suite.descriptions:
-    var r = this.runDescription(d, suite)
-    res.descriptions.add(r)
+  var res = SuiteResult(suite: suite.name, descriptions: @[])
 
-    res.testCount += r.testCount
-    res.succeeded += r.succeeded
-    res.skipped += r.skipped
-    res.failed += r.failed
+  # Run setup.
+  if suite.setup != nil:
+    try:
+      suite.setup()
+    except:
+      res.error = getCurrentException()
+      res.skipped += suite.testCount()
 
+  if res.error == nil:
+    for d in suite.descriptions:
+      var r = this.runDescription(d, suite)
+      res.descriptions.add(r)
+
+      res.testCount += r.testCount
+      res.succeeded += r.succeeded
+      res.skipped += r.skipped
+      res.failed += r.failed
+
+  if suite.teardown != nil:
+    try:
+      suite.teardown()
+    except:
+      res.error = getCurrentException()
+      
   res.timeTaken = epochTime() - startTime
 
   for r in this.reporters:
@@ -283,9 +351,12 @@ method onTestStarted(this: TerminalReporter, test: OmegaTest) =
 
 method onTestFinished(this: TerminalReporter, test: TestResult) =
   let name = test.suite & "." & test.description & "."
-  if test.status == SUCCESS:
+
+  case test.status
+  of SUCCESS:
     terminal.styledEcho(terminal.fgGreen, "*")
-  elif test.status == SKIPPED:
+
+  of SKIPPED:
     terminal.styledEcho(
       "\n",
       terminal.fgBlue, "#".repeat(80) & "\n", "# ",
@@ -296,12 +367,13 @@ method onTestFinished(this: TerminalReporter, test: TestResult) =
       test.skipReason, 
     )
     terminal.styledEcho(terminal.fgBlue, "#".repeat(80))
-  else:
+
+  of ERROR, BEFORE_EACH_ERR, AFTER_EACH_ERR:
     terminal.styledEcho(
       "\n",
       terminal.fgRed, "#".repeat(80) & "\n",
       terminal.fgRed, "# Test failed: \n#\t\n#\t", 
-      terminal.fgWhite, name,
+      terminal.fgWhite, "Test: ", name,
       terminal.fgRed, test.name, "\n#"
     )
 
@@ -315,6 +387,20 @@ method onTestFinished(this: TerminalReporter, test: TestResult) =
       terminal.styledEcho(terminal.fgRed, "#\t", terminal.fgWhite, line)
 
     terminal.styledEcho(terminal.fgRed, "#".repeat(80))
+  
+  of UNKNOWN:
+    terminal.styledEcho(
+      "\n",
+      terminal.fgRed, "#".repeat(80) & "\n",
+      terminal.fgRed, "# Test failed: \n#\t\n#\t", 
+      terminal.fgWhite, name,
+      terminal.fgRed, test.name, "\n#"
+    )
+
+    terminal.styledEcho(terminal.fgRed, "#\tReason:")
+    terminal.styledEcho(terminal.fgRed, "#\t  ", terminal.fgWhite, "Unknown")
+    terminal.styledEcho(terminal.fgRed, "#".repeat(80))
+
 
 method onDescriptionStarted(this: TerminalReporter, descr: OmegaDescription) =
   echo("\t$1: running $2 tests" % [descr.name, descr.testCount().`$`])
@@ -325,8 +411,24 @@ method onDescriptionFinished(this: TerminalReporter, res: DescriptionResult) =
 method onSuiteStarted(this: TerminalReporter, suite: OmegaSuite) =
   echo("\n$1: running $2 tests" % [suite.name, suite.testCount().`$`])
 
-method onSuiteFinished(this: TerminalReporter, res: SuiteResult) =
-  echo("")
+method onSuiteFinished(this: TerminalReporter, suite: SuiteResult) =
+  if suite.error != nil:
+    terminal.styledEcho(
+      "\n",
+      terminal.fgRed, "#".repeat(80) & "\n",
+      terminal.fgRed, "# Suite failed: \n#\t\n#\t",
+      terminal.fgWhite, "Suite: ",
+      terminal.fgRed, suite.suite, "\n#"
+    )
+    terminal.styledEcho(terminal.fgRed, "#\tReason:")
+    for line in suite.error.msg.splitLines():
+      terminal.styledEcho(terminal.fgRed, "#\t  ", terminal.fgWhite, line)
+    terminal.styledEcho(terminal.fgRed, "#\t")
+
+    for line in suite.error.getStackTrace().splitLines():
+      terminal.styledEcho(terminal.fgRed, "#\t", terminal.fgWhite, line)
+
+    terminal.styledEcho(terminal.fgRed, "#".repeat(80))
 
 method onStart(this: TerminalReporter, handler: Handler) =
   echo("Testing $1 suites with $2 tests." % [handler.suites.len().`$`, handler.testCount().`$`])
@@ -365,6 +467,7 @@ template Describe(descName: string, body: stmt): stmt {.immediate, dirty.} =
     if parentDescription == nil:
       suite.descriptions.add(description) 
     else:
+      description.parent = parentDescription
       parentDescription.descriptions.add(description)
 
     var parentDescription = description
@@ -416,6 +519,46 @@ when isMainModule:
 
       It("Should error"):
         raise newException(Exception, "some error")
+
+    Describe("beforeEach test"):
+      beforeEach:
+        raise newException(Exception, "beforeEach error")
+
+      It("Should fail in beforeEach"):
+        discard
+
+    Describe("afterEach test"):
+      afterEach:
+        raise newException(Exception, "some error")
+
+      It("Should fail in afterEach"):
+        discard
+
+  Suite("setupFail"):
+    setup:
+      raise newException(Exception, "some setup error")
+
+  Suite("teardownFail"):
+    teardown:
+      raise newException(Exception, "some teardown err")
+
+  Suite("Nested"):
+    Describe("D1"):
+      beforeEach:
+        echo("D1.beforeEach()")
+
+      afterEach:
+        echo("D1.afterEach()")
+
+      Describe("D1.1"):
+        beforeEach:
+          echo("D1.1.beforeEach()")
+
+        afterEach:
+          echo("D1.1.afterEach()")
+
+        It("Should succeed"):
+          discard
 
   discard Omega.run()
   #res.repr.echo
